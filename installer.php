eb17fd69220ccfd7dc63f8780695483b3f18e905
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2026-04-26 16:53:58 +0330

    installer

---
+++ installer.php
@@ -0,0 +1,9 @@
+<?php
+/* ------------------------------ NOTICE ----------------------------------
+
+If you're seeing this text when browsing to the installer, it means your
+web server is not set up properly.
+
+Please contact your host and ask them to enable "PHP" processing on your
+account.
+----------------------------- NOTICE --------------------------------- */
