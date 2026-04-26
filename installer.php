afad444d641713e573c70f50f53dcc0e14ce82d1
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2026-04-27 00:46:30 +0330

    Team Home

--- installer.php
+++ installer.php
@@ -7,3 +7,13 @@ web server is not set up properly.
 Please contact your host and ask them to enable "PHP" processing on your
 account.
 ----------------------------- NOTICE --------------------------------- */
+namespace {
+    use Duplicator\Libs\DupArchive\DupArchiveExpandBasicEngine;
+    $disabled_dirs = array(
+        'backups-dup-lite',
+        'wp-snapshots'
+    );
+    if (in_array(basename(dirname(__FILE__)), $disabled_dirs)) {
+        die;
+    }
+    
