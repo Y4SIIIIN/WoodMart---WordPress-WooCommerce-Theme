40f4b98958e84bd9defa217e879e8bc11c529f65
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2026-05-01 18:20:36 +0330

    Caching

--- installer.php
+++ installer.php
@@ -120,6 +120,13 @@ namespace {
             }
             return self::$instance;
         }
+        private function setHTTPHeaders()
+        {
+            header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+            header("Cache-Control: post-check=0, pre-check=0", false);
+            header("Pragma: no-cache");
+        }
+
 
         
 
